3158c0ea1bbf1ef4db3cf324d5e96fd17edd1dbe
[mohit_parmar_007] <[mohitparmar928@gmail.com]>
2024-07-24 23:26:38 +0530

    emegercy button fixed

--- client/src/components/Emergency.jsx
+++ client/src/components/Emergency.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 const Emergency = () => {
+  const handleEmergencyClick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const userData = {
+            user: {
+              success: true,
+              data: {
+                _id: '667f0a1bc127a2e2a9d45605',
+                firstName: 'mohit',
+                lastName: 'parmar',
+                email: 'jimohitji0786@gmail.com',
+                password: '123456',
+                picture: 'Dreamy Unseen Desktop 3D HD WorldFree4u.Com (51).JPG',
+                __v: 0
+              }
+            }
+          };
+
+          const locationData = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          };
+
+          const requestData = {
+            ...userData,
+            ...locationData,
+          };
+
+          axios.post('http://localhost:5000/emergency', requestData)
+            .then(response => {
+              console.log(response.data.message);
+            })
+            .catch(error => {
+              console.error('There was an error sending the location!', error);
+            });
+        },
+        (error) => {
+          console.error('Error getting location:', error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  };
+
   return (
     <div>
-        <button>call</button>
+      <button onClick={handleEmergencyClick}>Call</button>
     </div>
-  )
-}
+  );
+};
 
-export default Emergency
+export default Emergency;
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,23 +2,10 @@ import { useState } from "react";
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import axios from "axios"
+import axios from "axios";
 
 function Navbar() {
-
-  const handleEmergency = (e) => {
-    e.preventDefault();
-    axios
-        .post("http://localhost:5000/emergency", { user })
-        .then((res) => {
-            console.log(res);
-        
-        })
-        .catch((err) => console.log(err));
-};
-
   const user = useSelector((state) => state.user);
- 
   const [isActive, setIsActive] = useState(false);
 
   const toggleActiveClass = () => {
@@ -28,12 +15,52 @@ function Navbar() {
   const removeActive = () => {
     setIsActive(false);
   };
+
+  const handleEmergency = (e) => {
+    e.preventDefault();
+  
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const locationData = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          };
+  
+          const requestData = {
+            user: {
+              _id: user._id,
+              firstName: user.firstName,
+              lastName: user.lastName,
+              email: user.email,
+              picture: user.picture,
+            },
+            location: locationData,
+          };
+  
+          axios
+            .post("http://localhost:5000/emergency", requestData)
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => console.log(err));
+        },
+        (error) => {
+          console.error('Error getting location:', error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  };
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <nav className={`${styles.navbar}`}>
           <Link to="/" className={`${styles.logo}`}>
-            Travel Thrive{" "}
+            Travel Thrive
           </Link>
           <ul className={`${styles.navMenu} ${isActive ? styles.active : ""}`}>
             <li onClick={removeActive}>
@@ -53,32 +80,34 @@ function Navbar() {
             </li>
             <li onClick={removeActive}>
               <Link to="/EarnWithUS" className={`${styles.navLink}`}>
-              EarnWithUS
+                Earn With Us
               </Link>
             </li>
-            
           </ul>
           <div
             className={`${styles.hamburger} ${isActive ? styles.active : ""}`}
             onClick={toggleActiveClass}
           >
-           
-              {!user ? 
-                <div>
-                  <Link to="/login" className={`${styles.navLink}`}>
-                Login 
-              </Link>
-              <span> / </span>
-              <Link to="/register" className={`${styles.navLink}`}>
-                Signup
-              </Link>
-                </div> : 
-                <button onClick={handleEmergency} className="emgbtn" type="button">EMERGENCY</button>
-              }
+            {!user ? (
+              <div>
+                <Link to="/login" className={`${styles.navLink}`}>
+                  Login
+                </Link>
+                <span> / </span>
+                <Link to="/register" className={`${styles.navLink}`}>
+                  Signup
+                </Link>
+              </div>
+            ) : (
+              <button onClick={handleEmergency} className="emgbtn" type="button">
+                EMERGENCY
+              </button>
+            )}
           </div>
         </nav>
       </header>
     </div>
   );
 }
+
 export default Navbar;
